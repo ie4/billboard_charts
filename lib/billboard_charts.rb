d60c717244c2d0881665fe5397c68255a3c0825b
ie4 <kei@ie4.me>
2015-05-31 08:27:57 +0900

    add limit option

--- lib/billboard_charts.rb
+++ lib/billboard_charts.rb
@@ -5,20 +5,27 @@ require "nokogiri"
 
 class BillboardCharts
 
+  @limit = 100
+
   def initialize(chart_name='hot-100')
     url = 'http://www.billboard.com/charts/'+chart_name
     @scraped_data = Nokogiri::HTML(open(url))
   end
 
   def get_artists
-    @scraped_data.css(".chart-row h3").collect {|artist| artist.text.strip!}
+    @scraped_data.css(".chart-row h3").first(@limit).collect do |artist|
+      artist.text.strip!
+    end
   end
 
   def get_singles
-    @scraped_data.css(".chart-row h2").collect {|single| single.text.strip!}
+    @scraped_data.css(".chart-row h2").first(@limit).collect do |single|
+      single.text.strip!
+    end
   end
 
-  def get_list
+  def get_list(limit=100)
+    @limit=limit
     get_artists.zip(get_singles)
   end
 
